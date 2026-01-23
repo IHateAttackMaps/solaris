4b2e8301038632dce7870d2c36aa489f6b0d5e68
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-01-23 18:27:14 +0100

    Migrate report API to typescript

--- common/src/index.ts
+++ common/src/index.ts
@@ -54,6 +54,7 @@ export * from "./api/controllers/conversation";
 export * from "./api/controllers/research";
 export * from "./api/controllers/event";
 export * from "./api/controllers/trade";
+export * from "./api/controllers/report";
 export * from "./events/index";
 export * from "./events/handler";
 export * from "./events/emitter";
