0be5de65be04fd673a5d371c48c455d18f0fb143
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-08-11 14:02:15 +0200

    Fix spectator bar layout

--- client/src/views/game/components/menu/SpectatingWarningBar.vue
+++ client/src/views/game/components/menu/SpectatingWarningBar.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="bg-warning p-1" v-if="isSpectating">
-    <div class="row">
-      <div class="col">
-        <p class="mt-0 mb-0">You are spectating this game.</p>
-      </div>
-    </div>
-    <div class="row" v-if="isSpectatingDarkMode">
-      <div class="col">
-        <p class="mt-0 mb-0">
-          This is a <strong>dark mode</strong> game, no stars will be visible to
-          you unless you have been invited to spectate a player.
-        </p>
-      </div>
-    </div>
+    <p class="mt-0 mb-0">You are spectating this game.</p>
+    <p class="mt-0 mb-0" v-if="isSpectatingDarkMode">
+      This is a <strong>dark mode</strong> game, no stars will be visible to you
+      unless you have been invited to spectate a player.
+    </p>
   </div>
 </template>
 
